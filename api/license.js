00959a9c9c9cc664795831d74775cf10c7644b12
Ixxi14 <aguirreemerson750@gmail.com>
2026-04-10 16:47:47 -0600

    Update license.js

--- api/license.js
+++ api/license.js
@@ -6,7 +6,8 @@ export default function handler(req, res) {
     const { universeId } = req.body;
 
 const allowedUniverses = [
-    9918740473
+    9918740473,
+    9031635925
 ];
 
     if (!universeId) {
